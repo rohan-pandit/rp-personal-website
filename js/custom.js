bc7899e39eace678e897ce46be4b79e7a3128cfb
Rohan Pandit <rohanpandit@Rohans-MBP.fios-router.home>
2021-01-20 17:04:14 -0500

    modifying structure of html and adding sections and background, also added functionality for light mode and dark mode

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+var particle;
+
+ window.onload = function() {
+   		particle = Particles.init({
+     		 selector: '.background', connectParticles: 'true'
+    		});	
+  	};
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+
+  $('.mode-toggle').on('click', function() {
+
+      $('body').toggleClass("light-mode dark-mode");
+      if($('body.light-mode').length > 0){
+        particle = "";
+
+        particle = Particles.init({
+         selector: '.background', connectParticles: 'true', color: '#000000'
+        }); 
+      } else {
+          particle = "";
+
+          particle = Particles.init({
+           selector: '.background', connectParticles: 'true', color: '#FFFFFF'
+          }); 
+      }
+
+  });
+});
